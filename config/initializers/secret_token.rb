33ed0fc4a189ff17e19cf11cdf3cc684b0e9d67c
Ryo Yamada <ryoyamada3@gmail.com>
2015-02-13 17:21:17 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTurnipCapybaraSeleniumPoltergeist::Application.config.secret_key_base = '95ee1372cf5320a1675992689f87f8a428e1c30c0fbd50e7c94ccb803fc070357ae17a250adba5bffa08e41c540c1c81d948aa9ead11fcfafe077a39972f2f5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsTurnipCapybaraSeleniumPoltergeist::Application.config.session_store :cookie_store, key: '_rails_turnip_capybara_selenium_poltergeist_session'
